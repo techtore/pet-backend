af4aa15c42c0c0ce9c2c0bc5685a702d62fbd75b
Victoria Hale <vic.hale74@gmail.com>
2020-03-10 20:02:51 -0700

    remove homepage route and action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'homepage#index'
-  
+
   namespace :api do
     namespace :v1 do
       resources :daily_activities
@@ -11,4 +10,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
-# fetch 'http:localhost:3000/api/v1/dogs'
+# fetch 'http://localhost:3000/api/v1/dogs'
