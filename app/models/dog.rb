cd6b48ceb755f3afc09a5cc87ea879368018f853
Victoria Hale <vic.hale74@gmail.com>
2020-03-12 07:18:50 -0700

    remove nested attributes

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,4 @@
 class Dog < ApplicationRecord
     has_many :daily_activities
-    accepts_nested_attributes_for :daily_activities, allow_destroy: true
     validates :name, :age, :weight, :breed, presence: true
 end
