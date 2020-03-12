61dc9ca84495318d4a433a663cd506ce875b0232
Victoria Hale <vic.hale74@gmail.com>
2020-03-12 07:15:42 -0700

    set dog before action

--- app/controllers/api/v1/daily_activities_controller.rb
+++ app/controllers/api/v1/daily_activities_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::DailyActivitiesController < ApplicationController
   before_action :set_daily_activity, only: [:show, :update, :destroy]
+  before_action set_dog
 
   # GET /daily_activities
   def index
@@ -15,12 +16,12 @@ class Api::V1::DailyActivitiesController < ApplicationController
 
   # POST /daily_activities
   def create
-    @daily_activity = DailyActivity.new(daily_activity_params)
+    daily_activity = dog.daily_activities.new(daily_activity_params)
 
-    if @daily_activity.save
-      render json: @daily_activity, status: :created, location: @daily_activity
+    if daily_activity.save
+      render json: daily_activity, status: :created, location: daily_activity
     else
-      render json: @daily_activity.errors, status: :unprocessable_entity
+      render json: daily_activity.errors, status: :unprocessable_entity
     end
   end
 
@@ -42,6 +43,10 @@ class Api::V1::DailyActivitiesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_daily_activity
       @daily_activity = DailyActivity.find(params[:id])
+    end
+
+    def set_dog
+      dog = Dog.find_by(id: params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
